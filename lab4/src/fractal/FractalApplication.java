50988d5b8ecca1e6061b07d0071bdf968c58fc29
kevinnilsson <kevin7254@gmail.com>
2021-11-18 18:13:44 +0100

    Lab 4 done

--- lab4/src/fractal/FractalApplication.java
+++ lab4/src/fractal/FractalApplication.java
@@ -1,12 +1,16 @@
 package fractal;
 
 import koch.Koch;
+import mountain.MountainFractal;
+import mountain.Point;
+import mountain.RandomUtilities;
 
 public class FractalApplication {
 	public static void main(String[] args) {
-		Fractal[] fractals = new Fractal[1];
-		fractals[0] = new Koch(300);
+		Fractal[] fractals = new Fractal[2];
+		fractals[1] = new Koch(300);
+		fractals[0] = new MountainFractal(new Point(500,400), new Point(100,400), new Point(375,100), RandomUtilities.randFunc(20));
+		//new FractalView(fractals, "Mountain", 600, 600);
 	    new FractalView(fractals, "Fraktaler", 600, 600);
 	}
-
 }
